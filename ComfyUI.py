ca8384fe0c7fc8dbc095ff89023fb69ac48739f4
Firoz Ahmed <firofame@gmail.com>
2025-01-14 16:30:16 +0530

    toolkit

--- ComfyUI.py
+++ ComfyUI.py
@@ -2,11 +2,10 @@ import subprocess
 import modal
 import os
 
-os.environ["MODAL_ENVIRONMENT"] = "ComfyUI"
-
 image = modal.Image.debian_slim(python_version="3.11").apt_install("git", "unzip")
 image = image.pip_install("comfy-cli").run_commands("comfy --skip-prompt install --nvidia")
 image = image.pip_install("huggingface_hub[hf_transfer]").env({"HF_HUB_ENABLE_HF_TRANSFER": "1"}).run_commands("rm -rf /root/comfy/ComfyUI/models")
+image = image.run_commands("comfy node install https://github.com/crystian/ComfyUI-Crystools")
 image = image.run_commands("comfy node install https://github.com/city96/ComfyUI-GGUF")
 
 app = modal.App(name="ComfyUI", image=image)
@@ -30,6 +29,7 @@ def download_models():
         ("comfyanonymous/flux_text_encoders", "clip_l.safetensors", "clip"),
         ("black-forest-labs/FLUX.1-schnell", "ae.safetensors", "vae"),
         ("firofame/firoz", "firoz.safetensors", "loras"),
+        ("firofame/thilakan", "thilakan.safetensors", "loras"),
     ]
     list(hf_download.starmap(models_to_download))
 
---
+++ toolkit.py
@@ -0,0 +1,37 @@
+import subprocess
+import os
+from modal import (App, Image, web_server, Secret, Volume)
+
+os.environ["GRADIO_SERVER_NAME"] = "0.0.0.0"
+
+image = (
+    Image.debian_slim(python_version="3.11")
+    .apt_install("libgl1", "libglib2.0-0", "git")
+    .run_commands("git clone https://github.com/ostris/ai-toolkit.git /root/ai-toolkit")
+    .run_commands("cd /root/ai-toolkit && git submodule update --init --recursive")
+    .run_commands("pip install -r /root/ai-toolkit/requirements.txt")
+)
+
+image = image.pip_install("huggingface_hub[hf_transfer]").env({"HF_HUB_ENABLE_HF_TRANSFER": "1"}).run_commands("rm -rf /root/ai-toolkit/FLUX.1-dev")
+
+app = App("ai-toolkit", image=image, secrets=[Secret.from_name("huggingface-secret")])
+
+vol = Volume.from_name("toolkit-flux", create_if_missing=True)
+
+@app.function(volumes={"/root/models": vol})
+def hf_download(repo_id: str):
+    from huggingface_hub import snapshot_download
+    snapshot_download(repo_id=repo_id, local_dir="/root/models")
+
+@app.local_entrypoint()
+def download_models():
+    models_to_download = [
+        ("black-forest-labs/FLUX.1-dev",),
+    ]
+    list(hf_download.starmap(models_to_download))
+
+@app.function(allow_concurrent_inputs=2, concurrency_limit=1, container_idle_timeout=1200, timeout=1800, gpu="L40S", volumes={"/root/ai-toolkit/FLUX.1-dev": vol})
+@web_server(7860, startup_timeout=1800)
+def ui():
+    os.system("ls /root/ai-toolkit/FLUX.1-dev")
+    subprocess.Popen("cd /root/ai-toolkit && python flux_train_ui.py", shell=True)
