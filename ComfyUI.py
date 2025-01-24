dbfe89d6e5dcd20edec7326a660c515ac87ad043
Firoz Ahmed <Firoz.Ahmed@hcahealthcare.com>
2025-01-24 14:49:38 +0530

    HF_TOKEN

--- ComfyUI.py
+++ ComfyUI.py
@@ -1,7 +1,9 @@
 import subprocess
 import modal
+import os
 
 image = modal.Image.debian_slim(python_version="3.11").apt_install("git", "libgl1", "libglib2.0-0", "unzip")
+image = image.run_commands("pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu126")
 image = image.pip_install("comfy-cli").run_commands("comfy --skip-prompt install --nvidia")
 image = image.run_commands("comfy node install https://github.com/crystian/ComfyUI-Crystools")
 image = image.run_commands("comfy node install https://github.com/city96/ComfyUI-GGUF")
@@ -17,11 +19,12 @@ vol = modal.Volume.from_name("ComfyUI-models", create_if_missing=True)
 def rm_file():
     subprocess.run("rm -rf /root/models/vae/pytorch_lora_weights.safetensors", shell=True)
 
-@app.function(volumes={"/root/models": vol})
+@app.function(volumes={"/root/models": vol}, secrets=[modal.Secret.from_name("huggingface-secret")])
 def hf_download(repo_id: str, filename: str, model_type: str):
+    HF_TOKEN = os.environ["HF_TOKEN"]
     from huggingface_hub import hf_hub_download
     local_dir = f"/root/models/{model_type}"
-    hf_hub_download(repo_id=repo_id, filename=filename, local_dir=local_dir)
+    hf_hub_download(repo_id=repo_id, filename=filename, local_dir=local_dir, token=HF_TOKEN)
     
     if filename.endswith(".zip"):
         subprocess.run(["unzip", f"{local_dir}/{filename}", "-d", local_dir], check=True)
@@ -33,7 +36,7 @@ def download_models():
         ("city96/t5-v1_1-xxl-encoder-gguf", "t5-v1_1-xxl-encoder-Q8_0.gguf", "clip"),
         ("comfyanonymous/flux_text_encoders", "clip_l.safetensors", "clip"),
         ("black-forest-labs/FLUX.1-schnell", "ae.safetensors", "vae"),
-        ("firofame/flux-dreambooth-lora", "checkpoint-500/pytorch_lora_weights.safetensors", "loras"),
+        ("firofame/manju-warrier", "manju-warrier.safetensors", "loras"),
     ]
     list(hf_download.starmap(models_to_download))
 
