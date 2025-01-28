d4f0732532becbc55c85fc119cc0ea7232df6e29
Firoz Ahmed <Firoz.Ahmed@hcahealthcare.com>
2025-01-28 23:23:11 +0530

    faceid

--- ComfyUI.py
+++ ComfyUI.py
@@ -5,9 +5,9 @@ import os
 image = modal.Image.debian_slim(python_version="3.11").apt_install("git", "libgl1", "libglib2.0-0", "unzip")
 image = image.run_commands("pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu126")
 image = image.pip_install("comfy-cli").run_commands("comfy --skip-prompt install --nvidia")
-image = image.run_commands("comfy node install https://github.com/crystian/ComfyUI-Crystools")
-image = image.run_commands("comfy node install https://github.com/city96/ComfyUI-GGUF")
-image = image.run_commands("comfy node install https://github.com/kijai/ComfyUI-Florence2")
+image = image.pip_install("insightface", "onnxruntime")
+image = image.run_commands("comfy node install https://github.com/Gourieff/ComfyUI-ReActor")
+image = image.run_commands("comfy node install https://github.com/cubiq/ComfyUI_IPAdapter_plus")
 
 image = image.pip_install("huggingface_hub[hf_transfer]").env({"HF_HUB_ENABLE_HF_TRANSFER": "1"}).run_commands("rm -rf /root/comfy/ComfyUI/models")
 
@@ -32,15 +32,13 @@ def hf_download(repo_id: str, filename: str, model_type: str):
 @app.local_entrypoint()
 def download_models():
     models_to_download = [
-        ("city96/FLUX.1-dev-gguf", "flux1-dev-Q8_0.gguf", "unet"),
-        ("city96/t5-v1_1-xxl-encoder-gguf", "t5-v1_1-xxl-encoder-Q8_0.gguf", "clip"),
-        ("comfyanonymous/flux_text_encoders", "clip_l.safetensors", "clip"),
-        ("black-forest-labs/FLUX.1-schnell", "ae.safetensors", "vae"),
-        ("firofame/manju-warrier", "manju-warrier.safetensors", "loras"),
+        ("SG161222/Realistic_Vision_V5.1_noVAE", "Realistic_Vision_V5.1.safetensors", "checkpoints"),
+        ("stabilityai/sd-vae-ft-mse-original", "vae-ft-mse-840000-ema-pruned.safetensors", "vae"),
+        ("ezioruan/inswapper_128.onnx", "inswapper_128.onnx", "insightface"),
     ]
     list(hf_download.starmap(models_to_download))
 
-@app.function(allow_concurrent_inputs=2, concurrency_limit=1, container_idle_timeout=60*20, timeout=60*60*2, gpu="L4", volumes={"/root/comfy/ComfyUI/models": vol})
+@app.function(allow_concurrent_inputs=100, concurrency_limit=1, container_idle_timeout=60*20, timeout=60*60*2, gpu="T4", volumes={"/root/comfy/ComfyUI/models": vol})
 @modal.web_server(8188, startup_timeout=60*5)
 def ui():
     subprocess.Popen("comfy launch -- --listen", shell=True)
