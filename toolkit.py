2d9fa88bec895bd68f2270764e6ce1f420cd12bf
Firoz Ahmed <Firoz.Ahmed@hcahealthcare.com>
2025-01-28 15:30:17 +0530

    Update toolkit.py

--- toolkit.py
+++ toolkit.py
@@ -9,6 +9,7 @@ image = (
     .apt_install("libgl1", "libglib2.0-0", "git")
     .run_commands("git clone https://github.com/ostris/ai-toolkit.git /root/ai-toolkit")
     .run_commands("cd /root/ai-toolkit && git submodule update --init --recursive")
+    .run_commands("pip install -U torch")
     .run_commands("pip install -r /root/ai-toolkit/requirements.txt")
 )
 
@@ -18,17 +19,11 @@ app = App("ai-toolkit", image=image, secrets=[Secret.from_name("huggingface-secr
 
 vol = Volume.from_name("toolkit-flux", create_if_missing=True)
 
-@app.function(volumes={"/root/models": vol})
-def hf_download(repo_id: str):
-    from huggingface_hub import snapshot_download
-    snapshot_download(repo_id=repo_id, local_dir="/root/models")
-
 @app.local_entrypoint()
-def download_models():
-    models_to_download = [
-        ("black-forest-labs/FLUX.1-dev",),
-    ]
-    list(hf_download.starmap(models_to_download))
+@app.function(volumes={"/root/models": vol})
+def hf_download():
+    from huggingface_hub import snapshot_download
+    snapshot_download(repo_id="black-forest-labs/FLUX.1-dev", local_dir="/root/models")
 
 @app.function(allow_concurrent_inputs=2, concurrency_limit=1, container_idle_timeout=60*20, timeout=60*60, gpu="L40S", volumes={"/root/ai-toolkit/FLUX.1-dev": vol})
 @web_server(7860, startup_timeout=60*5)
