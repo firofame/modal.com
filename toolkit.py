87ae4bad1b487bff9c7dfe2d202c7c49f9b67766
Firoz Ahmed <firofame@gmail.com>
2025-01-30 11:22:51 +0530

    toolkit

--- toolkit.py
+++ toolkit.py
@@ -7,31 +7,23 @@ os.environ["GRADIO_SERVER_NAME"] = "0.0.0.0"
 image = (
     Image.debian_slim(python_version="3.11")
     .apt_install("libgl1", "libglib2.0-0", "git")
-    .run_commands("git clone https://github.com/ostris/ai-toolkit.git /root/ai-toolkit")
+    .run_commands("pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu126")
+    .run_commands("git clone -b fix/gradient-checkpointing --single-branch https://github.com/jhj0517/ai-toolkit.git /root/ai-toolkit")
     .run_commands("cd /root/ai-toolkit && git submodule update --init --recursive")
-    .run_commands("pip install -U torch")
     .run_commands("pip install -r /root/ai-toolkit/requirements.txt")
+    .pip_install("huggingface_hub[hf_transfer]")
+    .env({"HF_DATASETS_TRUST_REMOTE_CODE": "1", "HF_HUB_ENABLE_HF_TRANSFER": "1", "HF_HOME": "/cache"})
 )
-
-image = image.pip_install("huggingface_hub[hf_transfer]").env({"HF_HUB_ENABLE_HF_TRANSFER": "1"}).run_commands("rm -rf /root/ai-toolkit/FLUX.1-dev")
 
 app = App("ai-toolkit", image=image, secrets=[Secret.from_name("huggingface-secret")])
 
-vol = Volume.from_name("toolkit-flux", create_if_missing=True)
-
-@app.function(volumes={"/root/models": vol})
-def hf_download():
-    from huggingface_hub import snapshot_download
-    snapshot_download(
-        repo_id="black-forest-labs/FLUX.1-dev",
-        local_dir="/root/models"
-    )
+vol = Volume.from_name("hf-cache", create_if_missing=True)
 
 @app.local_entrypoint()
 def main():
-    hf_download.remote()
+    print("install complete")
 
-@app.function(allow_concurrent_inputs=2, concurrency_limit=1, container_idle_timeout=60*20, timeout=60*60, gpu="L40S", volumes={"/root/ai-toolkit/FLUX.1-dev": vol})
+@app.function(gpu="L40s", allow_concurrent_inputs=10, concurrency_limit=1, container_idle_timeout=60*20, timeout=60*30, volumes={"/cache": vol})
 @web_server(7860, startup_timeout=60*5)
 def ui():
     subprocess.Popen("cd /root/ai-toolkit && python flux_train_ui.py", shell=True)
