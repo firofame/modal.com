18c19dd1b04188bfdd853c85d200f9e7f0748176
Firoz Ahmed <Firoz.Ahmed@hcahealthcare.com>
2025-07-29 00:06:47 +0530

    Update comfi.py

--- comfi.py
+++ comfi.py
@@ -1,42 +1,40 @@
-import subprocess
 from pathlib import Path
+import subprocess
 import modal
-
-PORT = 8675
 
 image = (
     modal.Image.from_registry("nvidia/cuda:12.9.1-devel-ubuntu22.04", add_python="3.12")
-    .entrypoint([])  # remove verbose logging by base image on entry
+    .entrypoint([])
     .apt_install("git")
-    .uv_pip_install("huggingface-hub[hf-transfer]")
-    .run_commands("git clone https://github.com/comfyanonymous/ComfyUI /ComfyUI")
-    .run_commands("cd /ComfyUI && pip install -r requirements.txt")
+    .uv_pip_install("huggingface-hub[hf-transfer]", "comfy-cli")
+    .run_commands("comfy --skip-prompt install --fast-deps --nvidia")
+    .run_commands("comfy node install --fast-deps ComfyUI-Manager")
+    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
 )
 
-CACHE_DIR = Path("/cache")
-cache_volume = modal.Volume.from_name("hf-hub-cache", create_if_missing=True)
-volumes = {CACHE_DIR: cache_volume}
+def hf_download():
+    from huggingface_hub import hf_hub_download
+
+    kontext = hf_hub_download(repo_id="black-forest-labs/FLUX.1-Kontext-dev", filename="flux1-kontext-dev.safetensors", cache_dir="/cache")
+    subprocess.run(f"ln -s {kontext} /root/comfy/ComfyUI/models/diffusion_models/flux1-kontext-dev.safetensors", shell=True, check=True)
+
+    ae = hf_hub_download(repo_id="black-forest-labs/FLUX.1-Kontext-dev", filename="ae.safetensors", cache_dir="/cache")
+    subprocess.run(f"ln -s {ae} /root/comfy/ComfyUI/models/vae/ae.safetensors", shell=True, check=True)
+
+    clip_l = hf_hub_download(repo_id="comfyanonymous/flux_text_encoders", filename="clip_l.safetensors", cache_dir="/cache")
+    subprocess.run(f"ln -s {clip_l} /root/comfy/ComfyUI/models/text_encoders/clip_l.safetensors", shell=True, check=True)
+
+    t5xxl_fp16 = hf_hub_download(repo_id="comfyanonymous/flux_text_encoders", filename="t5xxl_fp16.safetensors", cache_dir="/cache")
+    subprocess.run(f"ln -s {t5xxl_fp16} /root/comfy/ComfyUI/models/text_encoders/t5xxl_fp16.safetensors", shell=True, check=True)
 
 secrets = [modal.Secret.from_name("huggingface-secret")]
+vol = modal.Volume.from_name("hf-hub-cache", create_if_missing=True)
+image = image.run_function(hf_download, volumes={"/cache": vol}, secrets=secrets)
 
+app = modal.App(name="comfy-ui", image=image)
 
-image = image.env(
-    {
-        "HF_HUB_ENABLE_HF_TRANSFER": "1",  # Allows faster model downloads
-        "HF_HOME": str(CACHE_DIR),  # Points the Hugging Face cache to a Volume
-    }
-)
-
-app = modal.App("comfy-ui", image=image, volumes=volumes, secrets=secrets)
-
-@app.function(
-    gpu="T4",
-    cpu=2,
-    memory=1024,
-    timeout=3600,
-    min_containers=1,  # Keep at least one instance of the server running.
-)
-@modal.concurrent(max_inputs=100)  # Allow 100 concurrent requests per container.
-@modal.web_server(port=PORT, startup_timeout=300)
-def run():
-    subprocess.Popen("cd /ComfyUI && python main.py --listen --port 8675", shell=True)
+@app.function(max_containers=1, gpu="T4", volumes={"/cache": vol})
+@modal.concurrent(max_inputs=10)
+@modal.web_server(8000, startup_timeout=60)
+def ui():
+    subprocess.Popen("comfy launch -- --listen 0.0.0.0 --port 8000", shell=True)
