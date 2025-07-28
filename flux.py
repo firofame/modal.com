b7c052c8a7f802b18fe103746ca1e04418fa9d6e
Firoz Ahmed <Firoz.Ahmed@hcahealthcare.com>
2025-07-28 07:57:44 +0530

    Update flux.py

--- flux.py
+++ flux.py
@@ -1,38 +1,78 @@
-import modal
-import os
+import time
 from io import BytesIO
 from pathlib import Path
 
-image = (
-    modal.Image.debian_slim(python_version="3.12")
-    .pip_install("sentencepiece", "peft", "transformers", "diffusers[torch]", "huggingface_hub[hf_transfer]")
-    .env({"HF_DATASETS_TRUST_REMOTE_CODE": "1", "HF_HUB_ENABLE_HF_TRANSFER": "1", "HF_HOME": "/cache"})
+import modal
+
+flux_image = (
+    modal.Image.from_registry("nvidia/cuda:12.9.1-devel-ubuntu22.04", add_python="3.12")
+    .entrypoint([])  # remove verbose logging by base image on entry
+    .apt_install("git")
+    .uv_pip_install("huggingface-hub[hf-transfer]")
+    .uv_pip_install("accelerate", "transformers", "git+https://github.com/huggingface/diffusers")
+    .uv_pip_install("peft", "sentencepiece")
 )
 
-app = modal.App(name="flux", image=image, secrets=[modal.Secret.from_name("huggingface-secret")])
+flux_image = flux_image.env(
+    {
+        "HF_HUB_ENABLE_HF_TRANSFER": "1",  # Allows faster model downloads
+        "HF_HOME": str(Path("/cache")),  # Points the Hugging Face cache to a Volume
+    }
+)
 
-vol = modal.Volume.from_name("hf-hub-cache", create_if_missing=True)
+app = modal.App("flux", image=flux_image)
 
-@app.function(gpu="L40s", volumes={"/cache": vol})
-def inference() -> bytes:
-    from huggingface_hub import login
-    login(os.environ["HF_TOKEN"])
-
-    from diffusers import AutoPipelineForText2Image
+with flux_image.imports():
     import torch
+    from diffusers import AutoPipelineForText2Image
 
-    pipe = AutoPipelineForText2Image.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
-    pipe.load_lora_weights("firofame/manjuw", weight_name="manjuw.safetensors")
-    pipe.fuse_lora(lora_scale=1.1)
-    pipe.to("cuda")
-    prompt = "Photograph of manjuw, A cute woman holding a sign that says Flux"
-    img = pipe(prompt=prompt, guidance_scale=3.5, height=768, width=512, num_inference_steps=30).images[0]
-    byte_stream = BytesIO()
-    img.save(byte_stream, format="PNG")
-    return byte_stream.getvalue()
+MINUTES = 60  # seconds
+secrets = [modal.Secret.from_name("huggingface-secret")]
+
+@app.cls(
+    gpu="L40S",
+    secrets=secrets,
+    scaledown_window=20 * MINUTES,
+    timeout=60 * MINUTES,  # leave plenty of time for compilation
+    volumes={  # add Volumes to store serializable compilation artifacts, see section on torch.compile below
+        "/cache": modal.Volume.from_name("hf-hub-cache", create_if_missing=True),
+    },
+)
+
+class Model:
+    compile: bool = (  # see section on torch.compile below for details
+        modal.parameter(default=False)
+    )
+
+    @modal.enter()
+    def enter(self):
+        pipe = AutoPipelineForText2Image.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16).to("cuda")
+        pipe.load_lora_weights("firofame/manjuw", weight_name="manjuw.safetensors")
+        pipe.fuse_lora(lora_scale=1.1)
+        self.pipe = pipe
+
+    @modal.method()
+    def inference(self, prompt: str) -> bytes:
+        print("🎨 generating image...")
+        out = self.pipe(
+            prompt,
+            output_type="pil",
+            num_inference_steps=30,
+        ).images[0]
+
+        byte_stream = BytesIO()
+        out.save(byte_stream, format="JPEG")
+        return byte_stream.getvalue()
 
 @app.local_entrypoint()
-def main():
-    image_bytes = inference.remote()
-    output_path = Path("./output.png")
+def main(
+    prompt: str = "Photograph of manjuw, A cute woman holding a sign that says Flux",
+):
+    t0 = time.time()
+    image_bytes = Model().inference.remote(prompt)
+    print(f"🎨 first inference latency: {time.time() - t0:.2f} seconds")
+
+    output_path = Path("./output.jpg")
+    print(f"🎨 saving output to {output_path}")
     output_path.write_bytes(image_bytes)
+
