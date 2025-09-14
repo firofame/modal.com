1c64dfe6efda5dc1b99811dda4cdc50230e42685
Firoz Ahmed <firofame@users.noreply.github.com>
2025-09-14 11:54:07 +0530

    Create denoise.py

---
+++ denoise.py
@@ -0,0 +1,70 @@
+# venv/bin/modal run denoise.py --file-path /Users/firozahmed/Downloads/GDvAHKZR87g.opus
+
+from io import BytesIO
+from pathlib import Path
+import tempfile
+
+import modal
+
+image = (
+    modal.Image.from_registry("nvidia/cuda:12.9.1-devel-ubuntu22.04", add_python="3.11")
+    .entrypoint([])
+    .apt_install("git", "git-lfs")
+    .uv_pip_install("resemble-enhance")
+)
+
+CACHE_DIR = Path("/cache")
+cache_volume = modal.Volume.from_name("hf-hub-cache", create_if_missing=True)
+volumes = {CACHE_DIR: cache_volume}
+
+secrets = [modal.Secret.from_name("huggingface-secret")]
+
+
+image = image.env({"HF_HUB_ENABLE_HF_TRANSFER": "1", "HF_HOME": str(CACHE_DIR)})
+
+
+app = modal.App("resemble-enhance")
+
+with image.imports():
+    import torchaudio
+    from resemble_enhance.enhancer.inference import denoise
+
+@app.cls(
+    image=image, gpu="T4", volumes=volumes, secrets=secrets, scaledown_window=240
+)
+class Model:
+    @modal.enter()
+    def enter(self):
+        self.device = "cuda"
+
+    @modal.method()
+    def inference(self, input_bytes: bytes, suffix:str) -> bytes:
+        with tempfile.NamedTemporaryFile(suffix=suffix) as temp_file:
+            temp_file.write(input_bytes)
+            temp_file.flush()
+
+            dwav, sr = torchaudio.load(temp_file.name)
+            dwav = dwav.mean(dim=0)
+
+            enhanced_wav, new_sr = denoise(dwav, sr, self.device)
+
+            buffer = BytesIO()
+            torchaudio.save(buffer, enhanced_wav.unsqueeze(0).cpu(), new_sr, format="wav")
+            return buffer.getvalue()
+
+
+@app.local_entrypoint()
+def main(file_path: str):
+    path = Path(file_path)
+    output_bytes = Model().inference.remote(input_bytes = path.read_bytes(), suffix=path.suffix)
+
+    output_path = "/Users/firozahmed/Downloads/output.wav"
+    
+    if isinstance(output_path, str):
+        output_path = Path(output_path)
+
+    dir = output_path.parent
+    dir.mkdir(exist_ok=True, parents=True)
+
+    output_path.write_bytes(output_bytes)
+    print(f"Enhanced audio saved to {output_path}")
