8146b7c4bf52ea167ad15635e4cbaa0cbdb181e1
Firoz Ahmed <firofame@users.noreply.github.com>
2025-09-15 20:29:38 +0530

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -7,18 +7,20 @@ image = (
     modal.Image.from_registry("nvidia/cuda:12.9.1-devel-ubuntu22.04", add_python="3.12")
     .entrypoint([])
     .apt_install("git", "ffmpeg", "libcudnn8", "libcudnn8-dev")
-    .pip_install("whisperx", "librosa")
+    .uv_pip_install("whisperx", "librosa", "numpy")
 )
+
 app = modal.App("whisperx", image=image)
 
-GPU_CONFIG = "T4"
-
 CACHE_DIR = "/cache"
-cache_vol = modal.Volume.from_name("whisper-cache", create_if_missing=True)
+device = "cuda"
+
+with image.imports():
+    import whisperx
 
 @app.cls(
-    gpu=GPU_CONFIG,
-    volumes={CACHE_DIR: cache_vol},
+    gpu="L40s",
+    volumes={CACHE_DIR: modal.Volume.from_name("whisper-cache", create_if_missing=True)},
     scaledown_window=60 * 10,
     timeout=60 * 60,
 )
@@ -26,10 +28,7 @@ cache_vol = modal.Volume.from_name("whisper-cache", create_if_missing=True)
 class Model:
     @modal.enter()
     def setup(self):
-        import whisperx
-
-        device = "cuda"
-        self.model = whisperx.load_model("large-v2", device, compute_type="float16", download_root=CACHE_DIR)
+        self.model = whisperx.load_model("large-v3", device, compute_type="float16", download_root=CACHE_DIR)
 
     @modal.method()
     def transcribe(self, audio_bytes: bytes):
@@ -38,6 +37,9 @@ class Model:
 
         audio, _ = librosa.load(io.BytesIO(audio_bytes), sr=16000)
         result = self.model.transcribe(audio, batch_size=16)
+
+        # model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+        # result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
 
         return result["segments"]
 
