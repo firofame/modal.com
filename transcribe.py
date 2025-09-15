97cc9a59792310659e92bb825fcedab1ae309dcc
Firoz Ahmed <firofame@users.noreply.github.com>
2025-09-15 17:23:16 +0530

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -5,9 +5,13 @@ import modal
 
 app = modal.App(name="whisper-transcribe-openai")
 
+MODEL_DIR = "/model"
+MODEL_NAME = "large-v3"
+volume = modal.Volume.from_name("whisper-model-vol", create_if_missing=True)
+
 image = modal.Image.debian_slim(python_version="3.12").uv_pip_install(
-    "openai-whisper==20250625",
-    "librosa==0.11.0",
+    "openai-whisper",
+    "librosa",
 )
 
 @app.local_entrypoint()
@@ -19,13 +23,21 @@ def main(file_path: str):
 @app.cls(
     image=image,
     gpu="L40s",
+    volumes={MODEL_DIR: volume},
+    scaledown_window=240,
+    timeout=600,
 )
 class Transcribe:
     @modal.enter()
     def load_model(self):
         import whisper
+        from pathlib import Path
 
-        self.model = whisper.load_model("large-v3")
+        model_path = Path(MODEL_DIR) / f"{MODEL_NAME}.pt"
+        if not model_path.exists():
+            whisper.load_model(MODEL_NAME, download_root=MODEL_DIR)
+            volume.commit()
+        self.model = whisper.load_model(MODEL_NAME, download_root=MODEL_DIR)
 
     @modal.method()
     def transcribe(
@@ -36,6 +48,6 @@ class Transcribe:
         import librosa
 
         audio_data, _ = librosa.load(io.BytesIO(audio_bytes), sr=16000)
-        transcription = self.model.transcribe(audio_data, language="ml")["text"]
+        transcription = self.model.transcribe(audio_data, language="ml", verbose=True)["text"]
 
         return transcription
