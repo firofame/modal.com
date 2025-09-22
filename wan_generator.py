c149768147873f94f131e898e78496ac0bd3d627
Firoz Ahmed <firofame@users.noreply.github.com>
2025-09-22 10:56:20 +0530

    Update wan_generator.py

--- wan_generator.py
+++ wan_generator.py
@@ -7,10 +7,19 @@ import subprocess
 
 image = (
     modal.Image.debian_slim(python_version="3.10")
-    .apt_install("git", "libgl1", "libglib2.0-0")
-    .run_commands("git clone https://github.com/deepbeepmeep/Wan2GP.git /Wan2GP")
+    .apt_install("git", "ffmpeg", "libgl1", "libglib2.0-0", "wget")
+    .run_commands(
+        "wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb",
+        "dpkg -i cuda-keyring_1.1-1_all.deb",
+        "apt-get update",
+        "apt-get -y install cuda-toolkit-12-8",
+    )
     .run_commands("pip install torch==2.7.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/test/cu128")
+    .run_commands("git clone https://github.com/deepbeepmeep/Wan2GP.git /Wan2GP")
     .run_commands("pip install -r /Wan2GP/requirements.txt")
+    .run_commands('python -m pip install "setuptools<=75.8.2" --force-reinstall')
+    .run_commands("git clone https://github.com/thu-ml/SageAttention.git /SageAttention")
+    .run_commands("pip install -e /SageAttention", gpu="L4")
 )
 
 app = modal.App("Wan2GP", image=image)
@@ -19,7 +28,7 @@ with image.imports():
     import warnings
     warnings.filterwarnings("ignore", category=FutureWarning)
 
-@app.function(max_containers=1, gpu="T4", volumes={"/cache": modal.Volume.from_name("hf-hub-cache", create_if_missing=True)})
+@app.function(max_containers=1, gpu="L4", volumes={"/cache": modal.Volume.from_name("hf-hub-cache", create_if_missing=True)})
 @modal.concurrent(max_inputs=10)
 @modal.web_server(7860, startup_timeout=600)
 def ui():
