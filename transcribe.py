0b53c87a9919323679e722cb812fffdbe12737ae
Firoz Ahmed <firofame@users.noreply.github.com>
2025-10-06 10:54:58 +0530

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -5,8 +5,6 @@ file_path = "https://www.youtube.com/shorts/xkD4JkcuP9M"
 # MODEL_NAME = "openai/whisper-large-v3"
 MODEL_NAME = "vrclc/Whisper-medium-Malayalam"
 
-gpu = "L4"
-
 from pathlib import Path
 import modal
 
@@ -18,43 +16,35 @@ image = (
     .env({"HF_HUB_ENABLE_HF_TRANSFER": "1", "HF_HOME": "/cache"})
 )
 
-app = modal.App("whisper-malayalam", image=image)
-
-with image.imports():
-    import torch
-    from transformers import pipeline
-    
-    import warnings
-    warnings.filterwarnings("ignore", category=UserWarning)
+app = modal.App("whisper", image=image)
 
 @app.cls(
-    gpu=gpu,
+    gpu="L4",
     volumes={"/cache": modal.Volume.from_name("hf-hub-cache", create_if_missing=True)},
     timeout=60 * 60,
     secrets=[modal.Secret.from_name("huggingface-secret")],
 )
 @modal.concurrent(max_inputs=15)
 class Model:
-    @modal.enter()
-    def setup(self):
-        self.pipe = pipeline(
-            "automatic-speech-recognition",
-            model=MODEL_NAME,
-            device="cuda",
-            chunk_length_s=30,
-            return_timestamps=True,
-            batch_size=8,
-            generate_kwargs={"task": "transcribe"},
-            ignore_warning=True,
-        )
-
     @modal.method()
     def transcribe(self, audio_bytes: bytes):
+        import torch
+        from transformers import pipeline
         import io
         import librosa
 
+
+        pipe = pipeline(
+            task="automatic-speech-recognition",
+            model=MODEL_NAME,
+            dtype=torch.float16,
+            device="cuda",
+            return_timestamps=True,
+            generate_kwargs={"task": "transcribe"},
+        )
+
         audio, _ = librosa.load(io.BytesIO(audio_bytes), sr=16000, mono=True)
-        result = self.pipe(audio)
+        result = pipe(audio)
 
         return result["text"]
 
@@ -64,7 +54,7 @@ def main():
     local_file_path = file_path
     if "youtube.com" in file_path or "youtu.be" in file_path:
         from yt_dlp import YoutubeDL
-        ydl_opts = {'quiet': True, 'postprocessors':[{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3'}], 'outtmpl': 'audio'}
+        ydl_opts = {'postprocessors':[{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3'}], 'outtmpl': 'audio'}
         YoutubeDL(ydl_opts).download([file_path])
         local_file_path = "audio.mp3"
     path = Path(local_file_path)
