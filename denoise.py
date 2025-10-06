0dff5f13d8590b22c26641da3f5cf06928b64750
Firoz Ahmed <firofame@users.noreply.github.com>
2025-10-06 08:08:20 +0530

    Update denoise.py

--- denoise.py
+++ denoise.py
@@ -1,4 +1,6 @@
-# venv/bin/modal run denoise.py --file-path /Users/firozahmed/Downloads/audio.ogg
+# venv/bin/modal run denoise.py
+
+file_path = "/Users/firozahmed/Downloads/audio.mpeg"
 
 from io import BytesIO
 from pathlib import Path
@@ -7,39 +9,22 @@ import tempfile
 import modal
 
 image = (
-    modal.Image.from_registry("nvidia/cuda:12.9.1-devel-ubuntu22.04", add_python="3.11")
-    .entrypoint([])
+    modal.Image.from_registry("pytorch/pytorch:2.8.0-cuda12.9-cudnn9-devel")
+    .run_commands("apt update")
     .apt_install("git", "git-lfs", "ffmpeg")
     .uv_pip_install("resemble-enhance")
 )
 
-CACHE_DIR = Path("/cache")
-cache_volume = modal.Volume.from_name("hf-hub-cache", create_if_missing=True)
-volumes = {CACHE_DIR: cache_volume}
-
-secrets = [modal.Secret.from_name("huggingface-secret")]
-
-
-image = image.env({"HF_HUB_ENABLE_HF_TRANSFER": "1", "HF_HOME": str(CACHE_DIR)})
-
-
 app = modal.App("resemble-enhance")
 
-with image.imports():
-    import torchaudio
-    from torchaudio.io import CodecConfig
-    from resemble_enhance.enhancer.inference import denoise
-
-@app.cls(
-    image=image, gpu="T4", volumes=volumes, secrets=secrets
-)
+@app.cls(image=image, gpu="T4")
 class Model:
-    @modal.enter()
-    def enter(self):
-        self.device = "cuda"
-
     @modal.method()
     def inference(self, input_bytes: bytes, suffix:str) -> bytes:
+        import torchaudio
+        from torchaudio.io import CodecConfig
+        from resemble_enhance.enhancer.inference import denoise
+
         with tempfile.NamedTemporaryFile(suffix=suffix) as temp_file:
             temp_file.write(input_bytes)
             temp_file.flush()
@@ -47,15 +32,15 @@ class Model:
             dwav, sr = torchaudio.load(temp_file.name)
             dwav = dwav.mean(dim=0)
 
-            enhanced_wav, new_sr = denoise(dwav, sr, self.device)
+            enhanced_wav, new_sr = denoise(dwav, sr, "cuda")
 
             buffer = BytesIO()
-            torchaudio.save(buffer, enhanced_wav.unsqueeze(0).cpu(), new_sr, format="opus", compression=CodecConfig(bit_rate=32000))
+            torchaudio.save(buffer, enhanced_wav.unsqueeze(0).cpu(), new_sr, format="opus")
             return buffer.getvalue()
 
 
 @app.local_entrypoint()
-def main(file_path: str):
+def main():
     path = Path(file_path)
     output_bytes = Model().inference.remote(input_bytes=path.read_bytes(), suffix=path.suffix)
     
